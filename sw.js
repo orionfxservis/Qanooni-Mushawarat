0b97f6ae8bd748e5df1fb5df12b6d48e5dc9fecf
Orion Fx <orionfxservis@gmail.com>
2026-06-04 19:25:09 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'qanooni-mushawarat-v1';
+const ASSETS = [
+    './index.html',
+    './css/style.css',
+    './images/logo/logo-03.png'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS).catch(() => {});
+        })
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((cachedResponse) => {
+            return cachedResponse || fetch(e.request);
+        })
+    );
+});
