d2dc7eefb220b42ed917f1cd680ff3ddec74e18e
Orion Fx <orionfxservis@gmail.com>
2026-06-07 17:34:01 +0500

    Add files via upload

---
+++ js/supabase.js
@@ -0,0 +1,9 @@
+// js/supabase.js
+
+const supabaseUrl = "https://aywuxnimzuqmocjccvbv.supabase.co/rest/v1/";
+const supabaseKey = "sb_publishable_rnxMaJuE7KAjchYt3VN53Q_lYuJQpW7";
+
+const supabase = window.supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
