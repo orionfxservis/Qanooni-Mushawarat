1cbd3fe766b39b68a0fcfe30adf38203da704e25
Orion Fx <orionfxservis@gmail.com>
2026-07-08 05:10:30 +0500

    Add files via upload

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 // js/supabase.js
 
-const supabaseUrl = "https://aywuxnimzuqmocjccvbv.supabase.co/rest/v1/";
+const supabaseUrl = "https://aywuxnimzuqmocjccvbv.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5d3V4bmltenVxbW9jamNjdmJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxMjAwMjAsImV4cCI6MjA5NTY5NjAyMH0.sEvlzk-Vl4TwS5rpR9mqcZ4AHE1RZgxOt9gPG6PJieQ";
 
 // Use a temporary variable name to prevent SyntaxError with the global SDK namespace
