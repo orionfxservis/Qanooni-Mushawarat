0c47115117cf0a1d06c6ceb6f08a67730aed19b6
Orion Fx <orionfxservis@gmail.com>
2026-07-18 15:48:53 +0500

    Add files via upload

---
+++ js/sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'qanooni-mushawarat-v32';
+const ASSETS = [
+    './index.html',
+    './css/style.css',
+    './images/logo/logo-03.png'
+];
+
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS).catch(() => { });
+        })
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+// Network-First Strategy: always fetch from network, fallback to cache if offline
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        fetch(e.request)
+            .then((response) => {
+                // If it is a valid response, cache a clone
+                if (response && response.status === 200) {
+                    const responseCopy = response.clone();
+                    caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(e.request, responseCopy);
+                    });
+                }
+                return response;
+            })
+            .catch(() => {
+                return caches.match(e.request);
+            })
+    );
+});
